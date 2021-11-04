2e3103205013a7e2791c045b0a740d6cb697f9d5
Konstantin Ezopov <k.ezopov@gmail.com>
2021-11-04 14:37:36 +0200

    comment -remove and edit

---
+++ src/Controller/CommentController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Comment;
+use App\Entity\Post;
+use App\Form\CommentType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/comment')]
+class CommentController extends AbstractController
+{
+    #[Route('/{id}/edit/', name: 'comment_edit', methods: ['Get', 'Post'])]
+    public function edit(Request $request, Comment $comment): Response
+    {
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('post_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('comment/edit.html.twig', [
+            'comment' => $comment,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/remove', name: 'comment_delete', methods: ['POST'])]
+    public function delete(Request $request, Comment $comment): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $comment->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($comment);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('post_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -17,6 +17,12 @@ class SecurityController extends AbstractController
         return $this->render('index.html.twig', ['posts' => $posts]);
     }
 
+    #[Route("/reg", name: "app_reg")]
+    public function reg(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
     #[Route("/login/", name: "app_login")]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
